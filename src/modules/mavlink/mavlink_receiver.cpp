5b5c97ea1428d454b142089b9fc26b3354ceb1ea
huiminc <cgz_05@sina.com>
2022-03-26 12:51:43 +0800

    feature: do not publish EV odometry until time sync has converged

--- src/modules/mavlink/mavlink_receiver.cpp
+++ src/modules/mavlink/mavlink_receiver.cpp
@@ -1204,6 +1204,8 @@ MavlinkReceiver::handle_message_set_gps_global_origin(mavlink_message_t *msg)
 void
 MavlinkReceiver::handle_message_vision_position_estimate(mavlink_message_t *msg)
 {
+	if (!_mavlink_timesync.sync_converged())
+		return;
 	mavlink_vision_position_estimate_t ev;
 	mavlink_msg_vision_position_estimate_decode(msg, &ev);
 
@@ -1243,6 +1245,8 @@ MavlinkReceiver::handle_message_vision_position_estimate(mavlink_message_t *msg)
 void
 MavlinkReceiver::handle_message_odometry(mavlink_message_t *msg)
 {
+	if (!_mavlink_timesync.sync_converged())
+		return;
 	mavlink_odometry_t odom;
 	mavlink_msg_odometry_decode(msg, &odom);
 
